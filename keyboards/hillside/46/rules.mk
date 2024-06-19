d2df096c54764cf8fc17e08efa6d81e26ffc159a
Wendy Chen <jaewchen@gmail.com>
2024-06-18 20:34:29 -0500

    Further cleaning up directory.

--- keyboards/hillside/46/rules.mk
+++ keyboards/hillside/46/rules.mk
@@ -7,3 +7,6 @@ MAGIC_ENABLE = no
 SPLIT_KEYBOARD = yes        # Use shared split_common code
 LTO_ENABLE = yes            # Use link time optimization for smaller firmware
 QMK_SETTINGS = no
+
+SRC += oneshot.c
+SRC += swapper.c
