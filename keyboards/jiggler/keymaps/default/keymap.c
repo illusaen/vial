c9e14005492d94fa90dde4fc2748abc7f2cb2112
illusaen <jaewchen@gmail.com>
2024-10-25 16:28:34 -0500

    deleting unnecessary stuff from jiggler

--- keyboards/jiggler/keymaps/default/keymap.c
+++ keyboards/jiggler/keymaps/default/keymap.c
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-#include "print.h"
+// #include "print.h"
 enum custom_keycodes {
   MOUSEJIGGLERMACRO
 };
@@ -22,10 +22,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  debug_keyboard=true;
-  debug_mouse=true;
+  // debug_enable=true;
+  // debug_matrix=true;
+  // debug_keyboard=true;
+  // debug_mouse=true;
 }
 
 uint16_t idle_timer = 0;
@@ -43,7 +43,7 @@ void matrix_scan_user(void) {
     tap_code(KC_MS_WH_UP);
     tap_code(KC_MS_WH_DOWN);
     idle_timer = timer_read();
-    dprintf("scanning %i %d %d!\n", random_idle_timer, elapsed_time, idle_timer);
+    // dprintf("scanning %i %d %d!\n", random_idle_timer, elapsed_time, idle_timer);
   }
 }
 
@@ -53,13 +53,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         if (mouse_jiggle_mode) {
             mouse_jiggle_mode = false;
-            dprint("mouse off\n");
+            // dprint("mouse off\n");
         } else {
             mouse_jiggle_mode = true;
-            dprint("mouse on\n");
+            // dprint("mouse on\n");
         }
       // SEND_STRING(SS_DELAY(100)); //uncomment if it switches too fast before the button debounces
-      } else {}
+      }
       break;
   }
   return true;
